c9394533b799e99514dbd908dff617fe6dca609e
Srikanth Bisai <94598179+srikanthbisai@users.noreply.github.com>
2024-12-04 18:19:05 +0530

    Update User.tsx

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Add as AddIcon } from "@mui/icons-material";
+import { Add as AddIcon } from '@mui/icons-material';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { 
